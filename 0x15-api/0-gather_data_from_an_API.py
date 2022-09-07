ec016eadc35ae1dea84349e89c263614da2dc7d5
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-09-06 19:24:45 -0700

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    user_id = argv[1]
-    url1 = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    url2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    us_id = argv[1]
+    url1 = "https://jsonplaceholder.typicode.com/users/{}".format(us_id)
+    url2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(us_id)
 
     name_resp = requests.get(url1).json()
     EMPLOYEE_NAME = name_resp.get('name')
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     directory = []
     for i in tasks_resp:
         if i.get("completed") is True:
-           directory.append(i.get("title"))
-           NUMBER_OF_DONE_TASKS += 1
+            directory.append(i.get("title"))
+            NUMBER_OF_DONE_TASKS += 1
     print("Employee {} is done with tasks({}/{}):"
-         .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for j in i:
         print("\t {}".format(j))
